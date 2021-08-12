53d69a66ae4d1cb1c577619880284807160e4e75
felipe.1092@gmail.com <felipe.1092@gmail.com>
2021-08-12 19:03:02 +0200

    Raw table preparation

--- rdocs/construccion de tabla.R
+++ rdocs/construccion de tabla.R
@@ -7,3 +7,8 @@ names(list_dv)=nombres
 
 for (i in nombres){list_dv[[i]]$origin_DB=i}
 table_form=do.call(rbind, list_dv)
+
+##ERASE COMPELTELY EMPTY COLUMNS NOTE LEAVES COLUMS THAT ARE "EMPTY" BUT IS NOT SIGNALED WITH A NA
+table_form=table_form[,which(unlist(lapply(table_form, function(x)!all(is.na(x)))))]
+
+write.csv(table_form, "data/TABLE_RAW.csv")
